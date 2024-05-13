8a94085119b8049cc133bee6220a30d66cadcbdd
Jozo_85 <67067963+Jozo233@users.noreply.github.com>
2024-05-13 17:28:08 +0200

    Add jozo.astro and tailwind

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+export const SOCIALS = [
+    {
+        name: "Github",
+        link: "https://github.com/Jozo233",
+        icon: "github.svg",
+    },
+    {
+        name: "Discord",
+        link: "https://github.com/Jozo233",
+        icon: "discord.svg"
+    }
+] satisfies {
+    name: string;
+    link: string;
+    icon: string;
+}[];
+
